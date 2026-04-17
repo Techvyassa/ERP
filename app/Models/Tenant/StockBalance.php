64dade220fe84c0d4e840818876d035fb721f381
develop-amit-arch <developer.amit25@gmail.com>
2026-04-17 11:45:49 +0530

    update the dashboard for production

--- app/Models/Tenant/StockBalance.php
+++ app/Models/Tenant/StockBalance.php
@@ -51,6 +51,11 @@ class StockBalance extends Model
         return $this->belongsTo(Material::class, 'material_id');
     }
 
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
     public function warehouse()
     {
         return $this->belongsTo(Warehouse::class, 'warehouse_id');
